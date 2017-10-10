a7811c18102f7706d4ac4184bbfb1d033b294055
Tom Dale <tdale@gannett.com>
2017-10-10 11:38:12 -0400

    added main page status test

--- test/test-page.js
+++ test/test-page.js
@@ -6,3 +6,9 @@ it('Main page content', function(){
     expect(body).to.equal('<h1>WOW SUPER COOL WEBPAGE</h1>');
   });
 });
+
+it('Main page status', function(){
+  request('http://localhost:3000/', function(err, res, body){
+  expect(res.status).to.equal(200);
+  });
+});
